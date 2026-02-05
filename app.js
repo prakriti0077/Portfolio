0d97522c85e5320c596c8c8f142c99f6df0cbc5f
Prakriti Patel <prakritipatel9@gmail.com>
2026-02-06 00:01:57 +0530

    Add portfolio website

---
+++ app.js
@@ -0,0 +1,9 @@
+﻿const links = document.querySelectorAll('a[href^="#"]');
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    const target = document.querySelector(link.getAttribute('href'));
+    if (!target) return;
+    e.preventDefault();
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
